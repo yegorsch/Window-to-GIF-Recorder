42d00e376809bd97c1b67513e11b3334b95edda2
Yegor Chsherbatykh <yegor.chsherbatykg@nu.edu.kz>
2017-05-07 21:34:56 +0600

    Initial Commit

---
+++ Wind/AppDelegate.swift
@@ -0,0 +1,26 @@
+//
+//  AppDelegate.swift
+//  Wind
+//
+//  Created by Егор on 5/7/17.
+//  Copyright © 2017 Yegor's Mac. All rights reserved.
+//
+
+import Cocoa
+
+@NSApplicationMain
+class AppDelegate: NSObject, NSApplicationDelegate {
+
+
+
+    func applicationDidFinishLaunching(_ aNotification: Notification) {
+        // Insert code here to initialize your application
+    }
+
+    func applicationWillTerminate(_ aNotification: Notification) {
+        // Insert code here to tear down your application
+    }
+
+
+}
+
---
+++ Wind/ViewController.swift
@@ -0,0 +1,27 @@
+//
+//  ViewController.swift
+//  Wind
+//
+//  Created by Егор on 5/7/17.
+//  Copyright © 2017 Yegor's Mac. All rights reserved.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override var representedObject: Any? {
+        didSet {
+        // Update the view, if already loaded.
+        }
+    }
+
+
+}
+
