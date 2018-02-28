858c668317b73f416f028167ff7adfe5e881dc79
Yegor Chsherbatykh <yegor.chsherbatykg@nu.edu.kz>
2018-03-01 00:39:42 +0600

    Wait time fixed

--- Wind/GifMaker.swift
+++ Wind/GifMaker.swift
@@ -23,6 +23,11 @@ class GifMaker{
     private let gifProperties:[String:Any]!
     private var dictInUse:CFDictionary!
     typealias SuccessBlock = (Bool)->()
+
+    let fileProperties: CFDictionary = [kCGImagePropertyGIFDictionary as String: [kCGImagePropertyGIFLoopCount as String: 0]]  as CFDictionary
+    var frameProperties: CFDictionary {
+        return [kCGImagePropertyGIFDictionary as String: [(kCGImagePropertyGIFDelayTime as String): 1 / self.fps]] as CFDictionary
+    }
     
     init(quality: Float,scale:Float, fps:Float, path:NSURL) {
         self.quality = quality / 100
@@ -37,17 +42,21 @@ class GifMaker{
     }
     
     func addImageIntoGif(image:CGImage){
-        self.processingQueue.async(group: self.dispatchGroup, qos: .background, flags: .enforceQoS, execute: {
+//        self.processingQueue.async(group: self.dispatchGroup, qos: .background, flags: .enforceQoS, execute: {
+//            self.images.append(image.resizeImage(level: self.quality, scale: self.scale))
+//        })
+        self.processingQueue.async {
             self.images.append(image.resizeImage(level: self.quality, scale: self.scale))
-        })
+        }
     }
     
     
     func generateGif(success: @escaping SuccessBlock){
         destinationGIF = CGImageDestinationCreateWithURL(self.path, kUTTypeGIF, self.images.count, nil)
+        CGImageDestinationSetProperties(destinationGIF, fileProperties)
         self.processingQueue.async(group: self.dispatchGroup, qos: .background, flags: .enforceQoS, execute: {
-            for image in self.images{
-                CGImageDestinationAddImage(self.destinationGIF, image, self.dictInUse)
+            while self.images.count > 0 {
+                CGImageDestinationAddImage(self.destinationGIF, self.images.removeFirst(), self.frameProperties)
             }
         })
         self.dispatchGroup.notify(queue: self.processingQueue, execute: {
